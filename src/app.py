4659f158f2012f079bbceb1d8a36467bac8d8ab2
khaledhawil <Khaledhawil91@gmail.com>
2025-05-27 21:25:38 +0300

    Edited in py file

--- src/app.py
+++ src/app.py
@@ -25,4 +25,4 @@ def home():
     return render_template('index.html', phrase=random_phrase)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
